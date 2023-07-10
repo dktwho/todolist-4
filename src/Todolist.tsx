6be770da5258e581bbab74b4bda61f1e8603b2ff
Alexander Korobov <dktwho@mail.ru>
2023-07-10 19:22:06 +0300

    add useState error, setError if value from input added falsy, classed input error - red and message text

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import {FilterValuesType} from './App';
+import styled from './Todolist.module.css'
 
 type TaskType = {
     id: string
@@ -19,12 +20,19 @@ type PropsType = {
 export function Todolist(props: PropsType) {
 
     let [title, setTitle] = useState("")
+    let [error, setError] = useState(false)
     const addTask = () => {
+        if (title === '') {
+            setError(true)
+            return
+        }
         props.addTask(title.trim());
         setTitle("");
+        setError(false)
     }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setError(false)
         setTitle(e.currentTarget.value)
     }
 
@@ -40,22 +48,24 @@ export function Todolist(props: PropsType) {
 
     return <div>
         <h3>{props.title}</h3>
+
         <div>
+
             <input value={title}
+                   className={error ? styled.error : ''}
                    onChange={onChangeHandler}
                    onKeyPress={onKeyPressHandler}
             />
             <button onClick={addTask}>+</button>
+            {error ? <h2 className={styled.errorMessage}>Value is required</h2> : null}
         </div>
         <ul>
             {
                 props.tasks.map(t => {
-
                     const onClickHandler = () => props.removeTask(t.id)
                     const onChangeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
                         props.changeCheckBoxStatus(t.id, e.currentTarget.checked)
                     }
-
                     return <li key={t.id}>
                         <input type="checkbox" checked={t.isDone} onChange={onChangeStatusHandler}/>
                         <span>{t.title}</span>
